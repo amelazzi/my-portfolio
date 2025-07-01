cb28a5353177afbbc176c9a107a47ea840bbed32
AAZ <amelias.azzi@gmail.com>
2025-07-01 11:36:59 +0200

    💄 About - Update description

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -9,10 +9,21 @@ function About() {
         <h1>Amel</h1>
         <h1>AZZI</h1>
       </div>
-      <div>
+      <div className="summary">
         <p>
-          Hello, I'm a fullstack developer based in Paris, France. I can develop
-          great webappas
+          As a <b>Senior Fullstack Engineer</b> with 5+ years of experience, I
+          specialize in building scalable applications using{" "}
+          <b>Node.js, React.js</b>, and a variety of modern technologies. With a
+          passion for creating impactful <b>user-centric solutions</b>, I thrive
+          in collaborative environments that challenge me to solve complex
+          problems.
+        </p>
+        <p>
+          As a <b>Google Women Techmakers Ambassador</b>, I am deeply committed
+          to advocating for diversity and inclusion in the tech industry. I
+          actively organize <b>community events</b> like{" "}
+          <b>Google Developer Groups</b> meetups to support and mentor aspiring
+          engineers, fostering the growth of <b>diverse talent</b> in tech.
         </p>
       </div>
 
