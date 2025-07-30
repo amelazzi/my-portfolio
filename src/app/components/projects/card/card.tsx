13db64bc384e9d4cf8e0a5e822e1070aa38e456c
AAZ <amelias.azzi@gmail.com>
2025-07-30 22:21:13 +0200

    💄 Update project card UI
    Add project info section

--- src/app/components/projects/card/card.tsx
+++ src/app/components/projects/card/card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import "./card.css";
 import Link from "next/link";
+import { ProjectDates } from "@/types/project.types";
 
 function Card({
   title,
@@ -9,6 +10,8 @@ function Card({
   role,
   logo,
   summary,
+  client,
+  dates,
   stacks,
 }: {
   title: string;
@@ -16,37 +19,53 @@ function Card({
   role: string;
   logo: string;
   summary: string;
+  client: string;
+  dates: ProjectDates;
   stacks: string[];
 }) {
   return (
     <div className="card">
       <div className="card-header">
+        <div className="card-logo">
+          <Image
+            src={logo}
+            width={75}
+            height={75}
+            style={{ borderRadius: "100%", objectFit: "contain" }}
+            alt={`${title} logo`}
+          />
+        </div>
         <div className="card-title">
           <div> {title} </div>
           <div> {role} </div>
         </div>
-        <div className="card-logo">
-          <Image
-            src={logo}
-            width={60}
-            height={60}
-            style={{ height: "auto" }}
-            alt={`azezaeaz logo`}
-          />
-        </div>
       </div>
       <div className="card-details">
-        <div className="card-sammury">{summary}</div>
-        <div className="card-stacks">
-          {stacks.map((stack, index) => (
-            <div key={index} className="card-stack">
-              {stack}
-            </div>
-          ))}
+        <div className="card-summary">{summary}</div>
+        <div className="card-footer">
+          <h4>Project Info</h4>
+          <hr />
+          <div className="project-info">
+            <p>Client</p>
+            <p>{client}</p>
+          </div>
+          <hr />
+          <div className="project-info">
+            <p>Dates</p>
+            <p>
+              {dates.start}-{dates.end}
+            </p>
+          </div>
+          <hr />
+          <div className="project-info">
+            <p>Stacks</p>
+            <p>{stacks.join(", ")} </p>
+          </div>
+          <hr />
         </div>
       </div>
       <Link href={`/projects/${slug}`} className="card-link">
-        More {">"}
+        Read More {">"}
       </Link>
     </div>
   );
