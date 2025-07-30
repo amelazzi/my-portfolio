a62e2b0c32de868fc87855c5f6419e54b015d714
AAZ <amelias.azzi@gmail.com>
2025-07-30 22:50:40 +0200

    💄 Make project card clickable

--- src/app/components/projects/card/card.tsx
+++ src/app/components/projects/card/card.tsx
@@ -24,50 +24,50 @@ function Card({
   stacks: string[];
 }) {
   return (
-    <div className="card">
-      <div className="card-header">
-        <div className="card-logo">
-          <Image
-            src={logo}
-            width={75}
-            height={75}
-            style={{ borderRadius: "100%", objectFit: "contain" }}
-            alt={`${title} logo`}
-          />
+    <Link href={`/projects/${slug}`}>
+      <div className="card">
+        <div className="card-header">
+          <div className="card-logo">
+            <Image
+              src={logo}
+              width={75}
+              height={75}
+              style={{ borderRadius: "100%", objectFit: "contain" }}
+              alt={`${title} logo`}
+            />
+          </div>
+          <div className="card-title">
+            <div> {title} </div>
+            <div> {role} </div>
+          </div>
         </div>
-        <div className="card-title">
-          <div> {title} </div>
-          <div> {role} </div>
+        <div className="card-details">
+          <div className="card-summary">
+            <p>{summary}</p>
+          </div>
+          <div className="card-footer">
+            <h4>Project Info</h4>
+            <hr />
+            <div className="project-info">
+              <p>Client</p>
+              <p>{client}</p>
+            </div>
+            <hr />
+            <div className="project-info">
+              <p>Dates</p>
+              <p>
+                {dates.start}-{dates.end}
+              </p>
+            </div>
+            <hr />
+            <div className="project-info">
+              <p>Stacks</p>
+              <p>{stacks.join(", ")} </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="card-details">
-        <div className="card-summary">{summary}</div>
-        <div className="card-footer">
-          <h4>Project Info</h4>
-          <hr />
-          <div className="project-info">
-            <p>Client</p>
-            <p>{client}</p>
-          </div>
-          <hr />
-          <div className="project-info">
-            <p>Dates</p>
-            <p>
-              {dates.start}-{dates.end}
-            </p>
-          </div>
-          <hr />
-          <div className="project-info">
-            <p>Stacks</p>
-            <p>{stacks.join(", ")} </p>
-          </div>
-          <hr />
-        </div>
-      </div>
-      <Link href={`/projects/${slug}`} className="card-link">
-        Read More {">"}
-      </Link>
-    </div>
+    </Link>
   );
 }
 
