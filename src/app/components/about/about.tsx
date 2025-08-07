7c35e94f9ea6db7ab52118e4accd6f9e01dffaad
AAZ <amelias.azzi@gmail.com>
2025-08-07 12:24:30 +0200

    💄 About - Display skills carousel

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 import Link from "next/link";
 import "./about.css";
+import { SkillsCarousel } from "../skills/skills-carousel/skills-carousel";
+import { skills } from "@/data/skills.data";
 
 function About() {
   return (
-    <div className="about">
-      <div className="full-name">
-        <h1>Amel</h1>
-        <h1>AZZI</h1>
+    <div className="about-section">
+      <div className="about">
+        <div className="full-name">
+          <h1>Amel</h1>
+          <h1>AZZI</h1>
+        </div>
+        <div className="summary">
+          <p>
+            As a <b>Senior Fullstack Engineer</b> with 5+ years of experience, I
+            specialize inbuilding scalable, user-focused applications using
+            <b>Node.js, React.js</b>, and
+            <b>modern web technologies.</b> I thrive in collaborative teams and
+            enjoy solving complex
+          </p>
+          <p>
+            As a <b>Google Women Techmakers Ambassador</b>, I am deeply
+            committed to advocating for diversity and inclusion in the tech
+            industry. I actively promote diversity in tech through organizing
+            GDG events and mentoring aspiring engineers.
+          </p>
+        </div>
+        <div className="about-carousel">
+          <SkillsCarousel skills={[...skills.Frontend]} cardSize={80} />
+          <p> ... and more</p>
+        </div>
+        <Link href="/contact">
+          <button>Contact Me</button>
+        </Link>
       </div>
-      <div className="summary">
-        <p>
-          As a <b>Senior Fullstack Engineer</b> with 5+ years of experience, I
-          specialize in building scalable applications using{" "}
-          <b>Node.js, React.js</b>, and a variety of modern technologies. With a
-          passion for creating impactful <b>user-centric solutions</b>, I thrive
-          in collaborative environments that challenge me to solve complex
-          problems.
-        </p>
-        <p>
-          As a <b>Google Women Techmakers Ambassador</b>, I am deeply committed
-          to advocating for diversity and inclusion in the tech industry. I
-          actively organize <b>community events</b> like{" "}
-          <b>Google Developer Groups</b> meetups to support and mentor aspiring
-          engineers, fostering the growth of <b>diverse talent</b> in tech.
-        </p>
-      </div>
-
-      <Link href="/contact">
-        <button>Contact Me</button>
-      </Link>
     </div>
   );
 }
