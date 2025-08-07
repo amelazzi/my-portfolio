de56da317e601ef76a9f189caf2a57bce424143e
AAZ <amelias.azzi@gmail.com>
2025-08-07 16:51:15 +0200

    ♻️  Profile - Refactor css & UI

--- src/app/components/profile/profile-wrapper/profile-wrapper.tsx
+++ src/app/components/profile/profile-wrapper/profile-wrapper.tsx
@@ -1,15 +1,13 @@
 import Profile from "../profile";
-import "./profile-wrapper.css";
+import "./profile-wrapper.scss";
 import { usePathname } from "next/navigation";
 
 export const ProfileWrapper = () => {
   const pathname = usePathname();
+  const isDefault = pathname !== "/about" && pathname !== "/";
+
   return (
-    <div
-      className={
-        `${pathname !== "/about" ? "default " : ""}` + "profile-container"
-      }
-    >
+    <div className={`profile-container ${isDefault ? "default-wrapper" : ""}`}>
       <Profile />
     </div>
   );
